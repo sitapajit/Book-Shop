7d4ea09bc99e1ca22a62e9fbddc6b11f90356a97
sitapa <sitapajit@gmail.com>
2016-06-21 11:23:20 +0700

    Design UI Login finish

--- app/src/main/java/neu/sitapa/bookshop/SignUpActivity.java
+++ app/src/main/java/neu/sitapa/bookshop/SignUpActivity.java
@@ -5,6 +5,16 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
+import com.squareup.okhttp.Call;
+import com.squareup.okhttp.Callback;
+import com.squareup.okhttp.FormEncodingBuilder;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
+import com.squareup.okhttp.Response;
+
+import java.io.IOException;
+
 public class SignUpActivity extends AppCompatActivity {
 
     //Explicit ประกาศตัวแปร
@@ -54,7 +64,33 @@ public class SignUpActivity extends AppCompatActivity {
 
     private void uploadToServer() {
 
-    }
+        OkHttpClient okHttpClient = new OkHttpClient();
+        RequestBody requestBody = new FormEncodingBuilder()
+                .add("isAdd", "true")
+                .add("Name", nameString)
+                .add("User", userString)
+                .add("Password", passwordString)
+                .build();
+        Request.Builder builder = new Request.Builder();
+        Request request = builder.url(urlPHP).post(requestBody).build();//จุดหมายปลายทางที่เราจะส่งดาต้าไป
+        Call call = okHttpClient.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Request request, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Response response) throws IOException {
+                finish();
+
+            }
+        });
+
+
+
+
+    }   //upload
 
 
 }   //Main Class
